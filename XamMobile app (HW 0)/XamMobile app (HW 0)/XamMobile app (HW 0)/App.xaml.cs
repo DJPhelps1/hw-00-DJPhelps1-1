590df05f38c751c60622e07ca80aa5fe5cceb585
DJPhelps1 <46822189+DJPhelps1@users.noreply.github.com>
2020-01-10 15:27:33 -0700

    Xamarin mobile app created.

---
+++ XamMobile app (HW 0)/XamMobile app (HW 0)/XamMobile app (HW 0)/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamMobile_app__HW_0_
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
